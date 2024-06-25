5aaf8af9f554ead26f4edcf824663e02e41b34c6
Preedie <kiadslawson@gmail.com>
2024-06-25 18:40:43 +1000

    CreateUser/Login/User/Movie/Add/Get

--- app.py
+++ app.py
@@ -1,20 +1,100 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
-from marshmallow import Schema, fields
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 
+# initialize application
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'your_postgresql_connection_string'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
 
+# config for JWT and Alchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = ''
+
+# Initialization of extensions
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
+# Route to login an existing user
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and bcrypt.check_password_hash(user.password, password):
+        access_token = create_access_token(identity={'username': username, 'email': user.email})
+        return jsonify({'access_token': access_token}), 200
+    else:
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+# User Info
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+# Route to create a new user
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(username=username, email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'})
+
+# Define Movie model
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    release_date = db.Column(db.Date)
+    rating = db.Column(db.Float)
+
+# Route to get all movies
+@app.route('/movies', methods=['GET'])
+def get_movies():
+    movies = Movie.query.all()
+    result = []
+    for movie in movies:
+        result.append({
+            'id': movie.id,
+            'title': movie.title,
+            'description': movie.description,
+            'release_date': movie.release_date.strftime('%Y-%m-%d') if movie.release_date else None,
+            'rating': movie.rating
+        })
+    return jsonify(result)
+
+# Route to add a new movie
+@app.route('/movies', methods=['POST'])
+@jwt_required()
+def add_movie():
+    data = request.get_json()
+    title = data.get('title')
+    description = data.get('description')
+    release_date_str = data.get('release_date')
+    release_date = datetime.strptime(release_date_str, '%Y-%m-%d').date() if release_date_str else None
+    rating = data.get('rating')
+
+    new_movie = Movie(title=title, description=description, release_date=release_date, rating=rating)
+    db.session.add(new_movie)
+    db.session.commit()
+    
+    return jsonify({'message': 'Movie added successfully'}), 201
+
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
-
-
